303d2636a90851b7dfd816803f3e186d13069b15
reiji-dat <reiji.dat@gmail.com>
2022-06-16 13:36:21 +0900

    kannsei

--- fukuvHensu/Form1.cs
+++ fukuvHensu/Form1.cs
@@ -17,5 +17,49 @@ namespace fukuvHensu
             InitializeComponent();
         }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + int.Parse(textBox1.Text);
+            label1.Top = label1.Top + int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "-5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "-5";
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "5";
+            textBox2.Text = "0";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "5";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show ("高橋涼");
+        }
     }
 }
